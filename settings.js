d1b3147c1e1c9130761368b1452c004d9e7e5dbe
safeinsight <127679199+safeinsight@users.noreply.github.com>
2026-09-13 14:43:08 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -131,6 +131,12 @@ async function authenticateSettingsUser() {
   console.log("settingsUser:", settingsUser);
   console.log("state.role:", state.role);
 
+  const { data: adminCheck, error: adminCheckError } =
+    await db.rpc("is_settings_administrator");
+
+  console.log("RLS ADMIN FUNCTION RESULT:", adminCheck);
+  console.log("RLS ADMIN FUNCTION ERROR:", adminCheckError);
+
   return true;
 }
 
