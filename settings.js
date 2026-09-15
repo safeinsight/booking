62733892684ae819f880602d524cf853e474f980
safeinsight <127679199+safeinsight@users.noreply.github.com>
2026-09-15 06:08:51 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -2209,9 +2209,7 @@ async function saveBookingRulesSettings(button) {
       throw settingsError;
     }
 
-    state.instructor = {
-      ...state.instructor,
-
+    const updatedBookingRules = {
       appointment_length_minutes:
         Number($("appointmentLengthInput").value),
 
@@ -2230,6 +2228,21 @@ async function saveBookingRulesSettings(button) {
       reschedule_hours:
         Number($("rescheduleHoursInput").value)
     };
+
+    state.instructor = {
+      ...state.instructor,
+      ...updatedBookingRules
+    };
+
+    state.instructors =
+      state.instructors.map(instructor =>
+        instructor.id === state.instructor.id
+          ? {
+              ...instructor,
+              ...updatedBookingRules
+            }
+          : instructor
+      );
 
     renderInstructorList();
 
